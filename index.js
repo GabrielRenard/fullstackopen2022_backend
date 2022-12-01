f9b52713d180b8533c6f9d569529a059ba1c7607
Gabriel Renard <gabriel.renard@protonmail.com>
2022-12-01 11:40:50 +0100

    3.3

--- index.js
+++ index.js
@@ -28,16 +28,22 @@ const info = `<p>Phonebook has info for ${
   persons.length
 } people</p><p>${new Date()}</p>`;
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World</h1>");
+app.get("/info", (request, response) => {
+  response.send(info);
 });
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
-app.get("/info", (request, response) => {
-  response.send(info);
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
 });
 
 const PORT = 3001;
