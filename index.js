24239a8c2ebfd7ffad468ce2616c39927add679d
Gabriel Renard <gabriel.renard@protonmail.com>
2022-12-01 11:35:02 +0100

    3.2

--- index.js
+++ index.js
@@ -24,12 +24,20 @@ const persons = [
   },
 ];
 
+const info = `<p>Phonebook has info for ${
+  persons.length
+} people</p><p>${new Date()}</p>`;
+
 app.get("/", (request, response) => {
   response.send("<h1>Hello World</h1>");
 });
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  response.send(info);
 });
 
 const PORT = 3001;
